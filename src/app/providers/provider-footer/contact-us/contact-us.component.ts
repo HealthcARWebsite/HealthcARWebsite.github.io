edb83e4ec17380f119512eabde0f8d4152b051f3
Wren <85448134+WrenPriest@users.noreply.github.com>
2023-03-28 21:39:07 -0500

    contact us form just needs correct URL

--- src/app/providers/provider-footer/contact-us/contact-us.component.ts
+++ src/app/providers/provider-footer/contact-us/contact-us.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
-import {MatDialogRef} from "@angular/material/dialog";
-import {ContactUsInterface} from "../../interfaces/contact-us-interface.interface";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import { Component } from '@angular/core';
+import { MatDialogRef } from "@angular/material/dialog";
+import { ContactUsInterface } from "../../interfaces/contact-us-interface.interface";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ProvidersService } from "../../providers.service";
+import { ContactUsFormInterface } from "../../interfaces/contact-us-form.interface";
 
 @Component({
   selector: 'app-contact-us',
@@ -10,18 +12,20 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
-  contactForm = new FormGroup({
+  contactForm = new FormGroup(<ContactUsFormInterface>{
     name: new FormControl('', Validators.required),
     email: new FormControl('', Validators.email),
     phone: new FormControl('', Validators.pattern('[- +()0-9]+')),
     organization: new FormControl(''),
     message: new FormControl('', Validators.required)
   })
-  protected data: ContactUsInterface = {name: '', email: '', phone: '', organization: '', message: ''}
+
+  // protected data: ContactUsInterface = {name: '', email: '', phone: '', organization: '', message: ''}
 
   constructor(
     public dialogRef: MatDialogRef<ContactUsComponent>,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar,
+    private providersService: ProvidersService) {
   }
 
   onNoClick(): void {
@@ -30,12 +34,14 @@ export class ContactUsComponent {
 
   onSubmit(): void {
     if (this.contactForm.valid) {
-      //TODO send post request
+      console.log(this.contactForm.value);
+      this.providersService.contactUs(this.contactForm.value as ContactUsInterface).subscribe({
+        next: () => this.snackBar.open('Message Sent', 'Close', {duration: 3000}),
+        error: () => this.snackBar.open('Message Failed', 'Close', {duration: 3000})
+      })
       this.dialogRef.close();
     } else {
-      this.snackBar.open('Invalid Form', 'Close', {
-        duration: 3000
-      });
+      this.snackBar.open('Invalid Form', 'Close', {duration: 3000});
     }
   }
 }
--- src/app/providers/providers.service.ts
+++ src/app/providers/providers.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { HealthcareProvider } from "./interfaces/healthcare-provider.interface";
 import { Observable } from "rxjs";
+import { ContactUsInterface } from "./interfaces/contact-us-interface.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProvidersService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   get healthcareProvider$(): Observable<HealthcareProvider[]> {
     return this.http.get<HealthcareProvider[]>('assets/mock-providers.json');
+  }
+
+  contactUs(formData: ContactUsInterface): Observable<any> {
+    //TODO add correct URL
+    return this.http.post<any>('fakeURL', JSON.stringify(formData));
   }
 }
