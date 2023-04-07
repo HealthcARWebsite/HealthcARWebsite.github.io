84da0bc1131a7d2b0ddc1f3cad49a00a9402f162
WrenPriest <wrenpriest@gmail.com>
2023-04-06 21:10:02 -0500

    started labelling snackBarMessages

--- src/app/providers/providers.service.ts
+++ src/app/providers/providers.service.ts
@@ -1,15 +1,24 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {HealthcareProvider} from "./interfaces/healthcare-provider.interface";
-import {BehaviorSubject, Observable} from "rxjs";
-import {ContactUsInterface} from "./interfaces/contact-us-interface.interface";
-import {ProviderSearchInterface} from "./interfaces/provider-search-interface.interface";
-import {EmailSearchResultsInterface} from "./interfaces/email-results-interface.interface";
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { HealthcareProvider } from "./interfaces/healthcare-provider.interface";
+import { BehaviorSubject, Observable } from "rxjs";
+import { ContactUsInterface } from "./interfaces/contact-us-interface.interface";
+import { ProviderSearchInterface } from "./interfaces/provider-search-interface.interface";
+import { EmailSearchResultsInterface } from "./interfaces/email-results-interface.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProvidersService {
+
+  public static readonly snackBarMessages = {
+    InvalidSearchTerms: $localize`:meaning|description:Invalid Search Terms`,
+    InvalidForm: $localize`Invalid Form`,
+    InvalidEmail: $localize`Invalid Email Address`,
+    MessageFailed: $localize`Message Failed`,
+    MessageSent: $localize`Message Sent`,
+    Close: $localize`Close`
+  }
 
   isTableLoading = false;
 
@@ -30,31 +39,31 @@ export class ProvidersService {
 
   updateHealthcareProviders(formData: ProviderSearchInterface) {
     this.isTableLoading = true;
-    const url = `${this.#baseUrl}/providers/get-providers`
+    const url = `${this.#baseUrl}/providers/get-providers`;
     this.http.get<HealthcareProvider[]>(url, {params: formData as any})
       .subscribe({
           next: (result) => {
             this.isTableLoading = false;
-            this.#healthcareProviders.next(result)
+            this.#healthcareProviders.next(result);
           },
           error: (e) => {
             this.isTableLoading = false;
             // pass empty array if there's an error
-            this.#healthcareProviders.next([])
-            console.error(e)
+            this.#healthcareProviders.next([]);
+            console.error(e);
           }
         }
       )
   }
 
   contactUs(formData: ContactUsInterface): Observable<any> {
-    const url = `${this.#baseUrl}/providers/contact-us`
+    const url = `${this.#baseUrl}/providers/contact-us`;
     return this.http.post<any>(url, formData);
   }
 
   emailSearchResults(formData: string): Observable<any> {
-    const url = `${this.#baseUrl}/providers/email`
-    this.emailSearchResultsFormData.emailAddress = formData
+    const url = `${this.#baseUrl}/providers/email`;
+    this.emailSearchResultsFormData.emailAddress = formData;
     return this.http.post<any>(url, this.emailSearchResultsFormData);
   }
 }
