dce8095a86bde2e182a5dad56877a4b5c13bbf1f
jkriffel <114178028+jkriffel@users.noreply.github.com>
2023-04-14 21:08:43 -0500

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/en-US'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/en-US/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
