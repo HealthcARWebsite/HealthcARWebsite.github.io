cd947e2926f3b907cd5eb11353b8d3ce7948f0b5
jkriffel <114178028+jkriffel@users.noreply.github.com>
2023-04-25 21:31:25 -0500

    es test

--- server.js
+++ server.js
@@ -1,59 +1,55 @@
-//const express = require('express');
-//const path = require('path');
-//const app = express();
-//app.use(express.static(__dirname + '/dist'));
-//app.get('/*', function(req,res) {
-//res.sendFile(path.join(__dirname+'/dist/index.html'));});
-//app.listen(process.env.PORT || 8080);
-
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const app = express();
+app.use(express.static(__dirname + '/dist/es'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/es/index.html'));});
+app.listen(process.env.PORT || 8080);
 
-console.log("First flag");
 
-function app() {
-  const server = express();
 
-  server.use(cookieParser());
 
-  const languages = ['en', 'es', 'mh'];
 
-  console.log("Second flag flag");
 
-  languages.forEach((locale) => {
-    const appServerModule = require(path.join(__dirname + locale + '/dist'));
-    server.use(`/${locale}`, appServerModule.app(locale));
-  });
 
-  console.log("Third flag");
 
-  server.get('/(:locale(en|es|mh)/)?*', (req, res, next) => {
-    const { urlLocale } = req.params;
-    const userLocale = (req.headers['accept-language'] || '').substring(0, 2);
-    
-    console.log("Fourth flag");
 
-    if (urlLocale !== userLocale) {
-      res.redirect(userLocale + req.url);
-    }
-  });
 
-  console.log("Fifth flag");
 
-  return server;
-}
 
-console.log("Sixth flag");
 
-function run() {
-  app().listen(4200, () => {
-    console.log(`Node Express server listening on http://localhost:4200`);
-  });
-}
+// const express = require('express');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
 
-console.log("Seventh flag");
+// function app() {
+//   const server = express();
 
-run();
+//   server.use(cookieParser());
 
-console.log("Eigth Flag")
+//   const languages = ['en', 'es', 'mh'];
+
+//   languages.forEach((locale) => {
+//     const appServerModule = require(path.join(__dirname , locale, '/dist'));
+//     server.use(`/${locale}`, appServerModule.app(locale));
+//   });
+
+//   server.get('/(:locale(en|es|mh)/)?*', (req, res, next) => {
+//     const { urlLocale } = req.params;
+//     const userLocale = (req.headers['accept-language'] || '').substring(0, 2);
+
+//     if (urlLocale !== userLocale) {
+//       res.redirect(userLocale + req.url);
+//     }
+//   });
+
+//   return server;
+// }
+
+// function run() {
+//   app().listen(4200, () => {
+//     console.log(`Node Express server listening on http://localhost:4200`);
+//   });
+// }
+
+// run();
