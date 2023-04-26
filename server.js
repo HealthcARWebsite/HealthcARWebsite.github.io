64a33cd95324a003939ddb5f3aab32e53fd28dbd
jkriffel <114178028+jkriffel@users.noreply.github.com>
2023-04-25 19:22:24 -0500

    First changes to build script and routing

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
+//const express = require('express');
+//const path = require('path');
+//const app = express();
+//app.use(express.static(__dirname + '/dist'));
+//app.get('/*', function(req,res) {
+//res.sendFile(path.join(__dirname+'/dist/index.html'));});
+//app.listen(process.env.PORT || 8080);
+
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/index.html'));});
-app.listen(process.env.PORT || 8080);
+const cookieParser = require('cookie-parser');
+
+function app() {
+  const server = express();
+
+  server.use(cookieParser());
+
+  const languages = ['en', 'es', 'mh'];
+
+  languages.forEach((locale) => {
+    const appServerModule = require(path.join(__dirname + '/dist' + locale));
+    server.use(`/${locale}`, appServerModule.app(locale));
+  });
+
+  server.get('/(:locale(en|es|mh)/)?*', (req, res, next) => {
+    const { urlLocale } = req.params;
+    const userLocale = (req.headers['accept-language'] || '').substring(0, 2);
+
+    if (urlLocale !== userLocale) {
+      res.redirect(userLocale + req.url);
+    }
+  });
+
+  return server;
+}
+
+function run() {
+  app().listen(4200, () => {
+    console.log(`Node Express server listening on http://localhost:4200`);
+  });
+}
+
+run();
