1b120fccf43211ceb8373c00855a424695be4ff6
jkriffel <114178028+jkriffel@users.noreply.github.com>
2023-04-25 21:01:09 -0500

    again

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
+console.log("First flag");
+
 function app() {
   const server = express();
 
@@ -17,22 +19,32 @@ function app() {
 
   const languages = ['en', 'es', 'mh'];
 
+  console.log("Second flag flag");
+
   languages.forEach((locale) => {
-    const appServerModule = require(path.join(__dirname , locale, '/dist'));
+    const appServerModule = require(path.join(__dirname + locale + '/dist'));
     server.use(`/${locale}`, appServerModule.app(locale));
   });
+
+  console.log("Third flag");
 
   server.get('/(:locale(en|es|mh)/)?*', (req, res, next) => {
     const { urlLocale } = req.params;
     const userLocale = (req.headers['accept-language'] || '').substring(0, 2);
+    
+    console.log("Fourth flag");
 
     if (urlLocale !== userLocale) {
       res.redirect(userLocale + req.url);
     }
   });
 
+  console.log("Fifth flag");
+
   return server;
 }
+
+console.log("Sixth flag");
 
 function run() {
   app().listen(4200, () => {
@@ -40,4 +52,8 @@ function run() {
   });
 }
 
+console.log("Seventh flag");
+
 run();
+
+console.log("Eigth Flag")
